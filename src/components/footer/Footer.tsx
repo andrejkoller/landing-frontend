b6ddc25352b31777ba07a173a926041f09c789ce
Andrej Koller <andrejkoller@outlook.com>
2025-08-17 16:34:12 +0200

    Refactor footer layout; simplify structure, enhance styling, and improve organization for better readability and responsiveness

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,139 +1,124 @@
 "use client";
 import styles from "./Footer.module.css";
 import { AnimatedLink } from "../AnimatedLink/AnimatedLink";
-import { LanguageSelector } from "../LanguageSelector/LanguageSelector";
-import Link from "next/link";
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
         <div className={styles.footerContent}>
-          <div className={styles.footerTop}>
-            <div className={styles.footerLogoContainer}>
-              <Link href={"/"} className={styles.footerLogo}>
-                Presenter
-              </Link>
+          <div className={styles.footerLinksContainer}>
+            <div className={styles.footerProductColumn}>
+              <h3 className={styles.footerProductTitle}>Products</h3>
+              <ul className={styles.footerProductLinks}>
+                <li className={styles.footerProductLink}>
+                  <AnimatedLink href={"/presenter"} hoverType={"color"}>
+                    Presenter
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerProductLink}>
+                  <AnimatedLink href={"/visuals"} hoverType={"color"}>
+                    Visuals
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerProductLink}>
+                  <AnimatedLink href={"/videoplayer"} hoverType={"color"}>
+                    Video Player
+                  </AnimatedLink>
+                </li>
+              </ul>
             </div>
-            <div className={styles.footerLinksContainer}>
-              <div className={styles.footerProductColumn}>
-                <h3 className={styles.footerProductTitle}>Products</h3>
-                <ul className={styles.footerProductLinks}>
-                  <li className={styles.footerProductLink}>
-                    <AnimatedLink href={"/presenter"} hoverType={"color"}>
-                      Presenter
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerProductLink}>
-                    <AnimatedLink href={"/visuals"} hoverType={"color"}>
-                      Visuals
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerProductLink}>
-                    <AnimatedLink href={"/videoplayer"} hoverType={"color"}>
-                      Video Player
-                    </AnimatedLink>
-                  </li>
-                </ul>
-              </div>
-              <div className={styles.footerCompanyColumn}>
-                <h3 className={styles.footerCompanyTitle}>Company</h3>
-                <ul className={styles.footerCompanyLinks}>
-                  <li className={styles.footerCompanyLink}>
-                    <AnimatedLink href={"/about"} hoverType={"color"}>
-                      About
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerCompanyLink}>
-                    <AnimatedLink href={"/contact"} hoverType={"color"}>
-                      Contact
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerCompanyLink}>
-                    <AnimatedLink href={"/privacy-policy"} hoverType={"color"}>
-                      Privacy Policy
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerCompanyLink}>
-                    <AnimatedLink
-                      href={"/terms-and-conditions"}
-                      hoverType={"color"}
-                    >
-                      Terms & Conditions
-                    </AnimatedLink>
-                  </li>
-                </ul>
-              </div>
-              <div className={styles.footerLearnColumn}>
-                <h3 className={styles.footerLearnTitle}>Learn</h3>
-                <ul className={styles.footerLearnLinks}>
-                  <li className={styles.footerLearnLink}>
-                    <AnimatedLink href={"/blog"} hoverType={"color"}>
-                      Blog
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerLearnLink}>
-                    <AnimatedLink href={"/tutorials"} hoverType={"color"}>
-                      Tutorials
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerLearnLink}>
-                    <AnimatedLink href={"/updates"} hoverType={"color"}>
-                      Updates
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerLearnLink}>
-                    <AnimatedLink href={"/features"} hoverType={"color"}>
-                      Features
-                    </AnimatedLink>
-                  </li>
-                </ul>
-              </div>
-              <div className={styles.footerSocialColumn}>
-                <h3 className={styles.footerSocialTitle}>Socials</h3>
-                <ul className={styles.footerSocialLinks}>
-                  <li className={styles.footerSocialLink}>
-                    <AnimatedLink href={"https://x.com"} hoverType={"color"}>
-                      X
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerSocialLink}>
-                    <AnimatedLink
-                      href={"https://linkedin.com"}
-                      hoverType={"color"}
-                    >
-                      LinkedIn
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerSocialLink}>
-                    <AnimatedLink
-                      href={"https://instagram.com"}
-                      hoverType={"color"}
-                    >
-                      Instagram
-                    </AnimatedLink>
-                  </li>
-                  <li className={styles.footerSocialLink}>
-                    <AnimatedLink
-                      href={"https://facebook.com"}
-                      hoverType={"color"}
-                    >
-                      Facebook
-                    </AnimatedLink>
-                  </li>
-                </ul>
-              </div>
+            <div className={styles.footerCompanyColumn}>
+              <h3 className={styles.footerCompanyTitle}>Company</h3>
+              <ul className={styles.footerCompanyLinks}>
+                <li className={styles.footerCompanyLink}>
+                  <AnimatedLink href={"/about"} hoverType={"color"}>
+                    About
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerCompanyLink}>
+                  <AnimatedLink href={"/contact"} hoverType={"color"}>
+                    Contact
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerCompanyLink}>
+                  <AnimatedLink href={"/privacy-policy"} hoverType={"color"}>
+                    Privacy Policy
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerCompanyLink}>
+                  <AnimatedLink
+                    href={"/terms-and-conditions"}
+                    hoverType={"color"}
+                  >
+                    Terms & Conditions
+                  </AnimatedLink>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.footerLearnColumn}>
+              <h3 className={styles.footerLearnTitle}>Learn</h3>
+              <ul className={styles.footerLearnLinks}>
+                <li className={styles.footerLearnLink}>
+                  <AnimatedLink href={"/blog"} hoverType={"color"}>
+                    Blog
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerLearnLink}>
+                  <AnimatedLink href={"/tutorials"} hoverType={"color"}>
+                    Tutorials
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerLearnLink}>
+                  <AnimatedLink href={"/updates"} hoverType={"color"}>
+                    Updates
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerLearnLink}>
+                  <AnimatedLink href={"/features"} hoverType={"color"}>
+                    Features
+                  </AnimatedLink>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.footerSocialColumn}>
+              <h3 className={styles.footerSocialTitle}>Socials</h3>
+              <ul className={styles.footerSocialLinks}>
+                <li className={styles.footerSocialLink}>
+                  <AnimatedLink href={"https://x.com"} hoverType={"color"}>
+                    X
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerSocialLink}>
+                  <AnimatedLink
+                    href={"https://linkedin.com"}
+                    hoverType={"color"}
+                  >
+                    LinkedIn
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerSocialLink}>
+                  <AnimatedLink
+                    href={"https://instagram.com"}
+                    hoverType={"color"}
+                  >
+                    Instagram
+                  </AnimatedLink>
+                </li>
+                <li className={styles.footerSocialLink}>
+                  <AnimatedLink
+                    href={"https://facebook.com"}
+                    hoverType={"color"}
+                  >
+                    Facebook
+                  </AnimatedLink>
+                </li>
+              </ul>
             </div>
           </div>
-          <div className={styles.footerMiddle}>
-            <div className={styles.footerCopyright}>
-              &copy; {new Date().getFullYear()} Presenter. All rights reserved.
-            </div>
-            <div className={styles.footerLanguageSelector}>
-              <LanguageSelector />
-            </div>
+          <div className={styles.footerCopyright}>
+            &copy; {new Date().getFullYear()} Presenter. All rights reserved.
           </div>
-          <div className={styles.footerBottom}></div>
         </div>
       </div>
     </footer>
