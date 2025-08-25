de7c17c0bb27c487616f307e3a055ecb735deb92
Andrej Koller <andrejkoller@outlook.com>
2025-08-25 18:21:47 +0200

    Refactor button styles; update font weight and family for AccountAddress, AccountEmail, AccountName, and AccountPhoneNumber components for improved consistency and appearance.

--- src/utils/buttonBaseSx.ts
+++ src/utils/buttonBaseSx.ts
@@ -2,4 +2,5 @@ export const buttonBaseSx = {
   padding: "12px 28.8px",
   border: "0.225rem solid var(--text-default)",
   borderRadius: "var(--border-radius-xxxl)",
+  fontFamily: "var(--font-inter)",
 };
