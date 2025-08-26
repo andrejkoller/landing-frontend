d38ca2fef92fe8e75093932fef2cd33dbbd9c168
Andrej Koller <andrejkoller@outlook.com>
2025-08-26 16:55:16 +0200

    Add logout and delete account buttons to AccountOptions component; update styles for improved layout and consistency.

--- src/components/AccountOptions/AccountOptions.tsx
+++ src/components/AccountOptions/AccountOptions.tsx
@@ -1,8 +1,9 @@
-import { FormControlLabel } from "@mui/material";
+import { ButtonBase, FormControlLabel } from "@mui/material";
 import styles from "./AccountOptions.module.css";
 import { AnimatedCheckbox } from "../AnimatedCheckbox/AnimatedCheckbox";
 import { ThemeSelect } from "../ThemeSelect/ThemeSelect";
 import { LanguageSelect } from "../LanguageSelect/LanguageSelect";
+import { LogOutIcon } from "lucide-react";
 
 export const AccountOptions = () => {
   return (
@@ -20,6 +21,44 @@ export const AccountOptions = () => {
             control={<AnimatedCheckbox />}
             label="Receive email updates about new versions before they are released"
           />
+        </div>
+        <div className={styles.accountField}>
+          <ButtonBase
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              padding: "16.5px 32px 16.5px 14px",
+              border: "1px solid var(--border-default)",
+              borderRadius: "4px",
+              width: "100%",
+              fontFamily: "Arial",
+              fontSize: "1rem",
+            }}
+            className={styles.accountLogoutButton}
+          >
+            <LogOutIcon />
+            <span className={styles.accountLogoutButtonText}>Logout</span>
+          </ButtonBase>
+        </div>
+        <div className={styles.accountField}>
+          <ButtonBase
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              padding: "16.5px 32px 16.5px 14px",
+              border: "1px solid var(--border-default)",
+              borderRadius: "4px",
+              color: "var(--color-red)",
+              width: "100%",
+              fontFamily: "Arial",
+              fontSize: "1rem",
+            }}
+            className={styles.accountDeleteButton}
+          >
+            <span className={styles.accountLogoutButtonText}>
+              Delete Account
+            </span>
+          </ButtonBase>
         </div>
       </div>
     </div>
