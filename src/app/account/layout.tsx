fcb13e480eeb1699aa58d27b93d230f48a254485
Andrej Koller <andrejkoller@outlook.com>
2025-08-27 16:28:54 +0200

    Implement authentication check in AccountLayout; redirect to signin if user is not authenticated.

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import { useAuth } from "@/hooks/useAuth";
 import styles from "./layout.module.css";
+import { useEffect } from "react";
 
 export default function AccountLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { publicUser } = useAuth();
+
+  useEffect(() => {
+    if (!publicUser) {
+      window.location.href =
+        "http://localhost:3001/signin?redirect=http://localhost:3000/callback";
+    }
+  }, [publicUser]);
   return <div className={styles.account}>{children}</div>;
 }
